3f1aa8b11767dec1b290eff1cbddb7e92eb9b77e
明高 王 <0sdf0@sina.cn>
2018-05-03 03:15:54 +0800

    demo

---
+++ DataBindingDemo/ChatViewModel.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace DataBindingDemo
+{
+    public class ChatViewModel
+    {
+        
+    }
+
+    //聊天列表
+    public class ChatList : ObservableCollection<Chat>
+    {
+
+    }
+
+    //好友列表
+    public class FriendList : ObservableCollection<Friend>
+    {
+
+    }
+
+    //通知列表
+    public class NoticeList : ObservableCollection<Notice>
+    {
+
+    }
+
+    public class Chat
+    {
+        public string SenderId { get; set; }
+
+        public string ReceiverId { get; set; }
+
+        public string Content { get; set; }
+    }
+
+    //每一个好友
+    public class Friend
+    {
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string IP { get; set; }
+
+        public Status status { get; set; }
+    }
+
+    //每一个通知
+    public class Notice
+    {
+        public string UserId { get;set; }
+    }
+
+    public enum Status
+    {
+        No = 0,
+        Yes = 1,
+        Online = 2,
+        Offline = 3,
+        Add = 4,
+        Remove = 5,
+        Agree = 6,
+        DisAgree = 7,
+        Waiting = 8
+    }
+
+        //每一项聊天
+        //public class Chat : INotifyPropertyChanged
+        //{
+        //    public event PropertyChangedEventHandler PropertyChanged;
+        //    protected void Notify(string propName)
+        //    {
+        //        if(this.PropertyChanged != null)
+        //        {
+        //            PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        //        }
+        //    }
+
+        //    public string SenderId
+        //    {
+        //        get { return SenderId; }
+        //        set
+        //        {
+        //            if(this.SenderId == value) { return; }
+        //            this.SenderId = value;
+        //            Notify("SenderId");
+        //        }
+        //    }
+
+        //    public string ReceiverId
+        //    {
+        //        get { return ReceiverId; }
+        //        set
+        //        {
+        //            if(this.ReceiverId == value) { return; }
+        //            this.ReceiverId = value;
+        //            Notify("ReceiverId");
+        //        }
+        //    }
+
+        //    public string Content
+        //    {
+        //        get { return Content; }
+        //        set
+        //        {
+        //            if(this.Content == value) { return; }
+        //            this.Content = value;
+        //            Notify("Content");
+        //        }
+        //    }
+        //}
+
+    }
--- DataBindingDemo/MainWindow.xaml.cs
+++ DataBindingDemo/MainWindow.xaml.cs
@@ -20,9 +20,84 @@ namespace DataBindingDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        public FriendList friends;
+        public ChatList chats;
+        public NoticeList notices;
         public MainWindow()
         {
+            friends = new FriendList
+            {
+                new Friend{UserId = "1234", UserName="wang", IP="192.168.1.1", status=Status.Online},
+                new Friend{UserId = "2345", UserName="ming", IP="192,168.1.2", status=Status.Online},
+                new Friend{UserId = "3456", UserName="gao", IP="192.168.1.3", status=Status.Offline}
+
+            };
+            chats = new ChatList
+            {
+                new Chat{SenderId = "1234", ReceiverId = "1111", Content = "测试测试测试测试测试测试"},
+                new Chat{SenderId = "2345", ReceiverId = "1111", Content = "测试测试测试测试测试"},
+                new Chat{SenderId = "3456", ReceiverId = "1111", Content= "测试测试测试测试"}
+            };
+            notices = new NoticeList
+            {
+                new Notice{UserId = "5678"},
+                new Notice{UserId = "4567"}
+            };
+
             InitializeComponent();
+
+            this.gChat.DataContext = chats;
+            this.gFriend.DataContext = friends;
+            this.gNotice.DataContext = notices;
         }
+
+        #region 测试用按钮
+        private void btnChatAddOne_Click(object sender, RoutedEventArgs e)
+        {
+            ellChat.Visibility = (ellChat.Visibility == Visibility.Collapsed) ? Visibility.Visible : Visibility.Visible;
+            chats.Add(new Chat { SenderId = "8888", ReceiverId = "6666", Content = "测试测试" });
+        }
+
+        private void btnChatDelOne_Click(object sender, RoutedEventArgs e)
+        {
+            chats.Remove(chats.Where(p => p.SenderId.Equals("8888")).FirstOrDefault());
+        }
+
+        private void btnFriendAddOne_Click(object sender, RoutedEventArgs e)
+        {
+            ellFriend.Visibility = (ellFriend.Visibility == Visibility.Collapsed) ? Visibility.Visible : Visibility.Visible;
+            friends.Add(new Friend { UserId = "8888", UserName = "测试", IP = "192.168.1.1", status = Status.Offline });
+        }
+
+        private void btnNoticeAddOne_Click(object sender, RoutedEventArgs e)
+        {
+            ellNotice.Visibility = (ellNotice.Visibility == Visibility.Collapsed) ? Visibility.Visible : Visibility.Visible;
+            notices.Add(new Notice { UserId="7777" });
+        }
+
+        private void btnFriendDelOne_Click(object sender, RoutedEventArgs e)
+        {
+            friends.Remove(friends.Where(p => p.UserId.Equals("8888")).FirstOrDefault());
+        }
+
+        private void btnNoticeDelOne_Click(object sender, RoutedEventArgs e)
+        {
+            notices.Remove(notices.Where(p => p.UserId.Equals("7777")).FirstOrDefault());
+        }
+        #endregion
     }
+
+    //[ValueConversion(typeof(string), typeof(string))]
+    //public class Id2Name : IValueConverter
+    //{
+    //    public object Convert(object value, Type TargetType)
+    //    {
+    //        if(TargetType != typeof(string)) { return null; }
+
+    //    }
+    //    public object ConvertBack(object value, Type targetType)
+    //    {
+    //        if(targetType != typeof(string)) { return null; }
+    //    }
+    //}
 }
