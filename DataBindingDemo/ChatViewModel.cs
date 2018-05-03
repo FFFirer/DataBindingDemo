8188b35219b9d60923ad20a5c78e65778e22f312
明高 王 <0sdf0@sina.cn>
2018-05-03 21:16:52 +0800

    完成界面的逻辑开发

--- DataBindingDemo/ChatViewModel.cs
+++ DataBindingDemo/ChatViewModel.cs
@@ -8,15 +8,56 @@ using System.Collections.ObjectModel;
 
 namespace DataBindingDemo
 {
-    public class ChatViewModel
+    public class ChatViewModel : INotifyPropertyChanged
     {
-        
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void Notify(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        private int allchatwaitreads { get; set; }
+        public int AllChatWaitReads
+        {
+            get { return this.allchatwaitreads; }
+            set
+            {
+                if(allchatwaitreads == value) { return; }
+                allchatwaitreads = value;
+                Notify("allchatwaitreads");
+            }
+        }
+        private int allfriendwaitreads { get; set; }
+        public int AllFriendWaitReads
+        {
+            get { return this.allfriendwaitreads; }
+            set
+            {
+                if (allfriendwaitreads == value) { return; }
+                allfriendwaitreads = value;
+                Notify("allfriendwaitreads");
+            }
+        }
+        private int allnoticewaitreads { get; set; }
+        public int AllNoticeWaitReads
+        {
+            get { return this.allnoticewaitreads; }
+            set
+            {
+                if (allnoticewaitreads == value) { return; }
+                allnoticewaitreads = value;
+                Notify("allnoticewaitreads");
+            }
+        }
     }
 
     //聊天列表
-    public class ChatList : ObservableCollection<Chat>
+    public class ChatList : ObservableCollection<Chat>, INotifyPropertyChanged
     {
-
+        
     }
 
     //好友列表
@@ -31,13 +72,59 @@ namespace DataBindingDemo
 
     }
 
-    public class Chat
+    //聊天记录
+    public class MsgList : ObservableCollection<PerMsg>
     {
+
+    }
+
+    public class Chat : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void Notify(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
         public string SenderId { get; set; }
+
+        public string sName { get; set; }
 
         public string ReceiverId { get; set; }
 
         public string Content { get; set; }
+
+        public MsgList Msgs { get; set; }
+
+        public void AddMsg(PerMsg msg)
+        {
+            Msgs.Add(msg);
+        }
+
+        private int waitreads { get; set; }
+        public int WaitReads
+        {
+            get { return waitreads; }
+            set
+            {
+                if (this.waitreads == value) { return; }
+                this.waitreads = value;
+                Notify("waitreads");
+            }
+        }
+
+        public Chat(string id, string name, string receId, string content, MsgList ml, int wait)
+        {
+            this.SenderId = id;
+            this.sName = name;
+            this.ReceiverId = receId;
+            this.Content = content;
+            this.Msgs = ml;
+            this.WaitReads = wait;
+        }
     }
 
     //每一个好友
@@ -69,6 +156,18 @@ namespace DataBindingDemo
         Agree = 6,
         DisAgree = 7,
         Waiting = 8
+    }
+
+    public class PerMsg
+    {
+        public string SenderName { get; set; }
+        public string Msg { get; set; }
+        public PerMsg() { }
+        public PerMsg(string SenderName, string Msg)
+        {
+            this.SenderName = SenderName;
+            this.Msg = Msg;
+        }
     }
 
         //每一项聊天
--- DataBindingDemo/MainWindow.xaml.cs
+++ DataBindingDemo/MainWindow.xaml.cs
@@ -20,11 +20,38 @@ namespace DataBindingDemo
     /// </summary>
     public partial class MainWindow : Window
     {
-        public FriendList friends;
-        public ChatList chats;
-        public NoticeList notices;
+        private string chating = null;
+        public ChatViewModel model = null;
+        public FriendList friends = null;
+        public ChatList chats = null;
+        public NoticeList notices = null;
         public MainWindow()
         {
+            PerMsg p1 = new PerMsg("wang", "ceshi");
+            PerMsg p2 = new PerMsg("wang", "ceshi");
+            PerMsg p3 = new PerMsg("wang", "ceshi");
+            MsgList m1 = new MsgList();
+            m1.Add(p1);
+            m1.Add(p2);
+            m1.Add(p3);
+
+            PerMsg p4 = new PerMsg("ming", "ceshi");
+            PerMsg p5 = new PerMsg("ming", "ceshi");
+            PerMsg p6 = new PerMsg("ming", "ceshi");
+            MsgList m2 = new MsgList();
+            m2.Add(p4);
+            m2.Add(p5);
+            m2.Add(p6);
+
+            PerMsg p7 = new PerMsg("gao", "ceshi");
+            PerMsg p8 = new PerMsg("gao", "ceshi");
+            PerMsg p9 = new PerMsg("gao", "ceshi");
+            MsgList m3 = new MsgList();
+            m3.Add(p7);
+            m3.Add(p8);
+            m3.Add(p9);
+
+
             friends = new FriendList
             {
                 new Friend{UserId = "1234", UserName="wang", IP="192.168.1.1", status=Status.Online},
@@ -32,30 +59,48 @@ namespace DataBindingDemo
                 new Friend{UserId = "3456", UserName="gao", IP="192.168.1.3", status=Status.Offline}
 
             };
+            
             chats = new ChatList
             {
-                new Chat{SenderId = "1234", ReceiverId = "1111", Content = "测试测试测试测试测试测试"},
-                new Chat{SenderId = "2345", ReceiverId = "1111", Content = "测试测试测试测试测试"},
-                new Chat{SenderId = "3456", ReceiverId = "1111", Content= "测试测试测试测试"}
+                //new Chat{SenderId = "1234", sName="wang", ReceiverId = "1111", Content = "测试测试测试测试测试测试", Msgs = m1, WaitReads = 3},
+                //new Chat{SenderId = "2345", sName="ming", ReceiverId = "1111", Content = "测试测试测试测试测试", Msgs = m2,WaitReads = 3},
+                //new Chat{SenderId = "3456", sName="gao", ReceiverId = "1111", Content= "测试测试测试测试", Msgs = m3,WaitReads = 3}
+                new Chat("1234", "wang", "1111", "测试测试测试测试测试测试", m1, 0),
+                new Chat("2345", "ming", "1111", "测试测试测试测试测试", m2, 0),
+                new Chat("3456", "gao", "1111", "测试测试测试测试", m3, 0),
+
             };
             notices = new NoticeList
             {
                 new Notice{UserId = "5678"},
                 new Notice{UserId = "4567"}
             };
+            model = new ChatViewModel { AllChatWaitReads = 0, AllFriendWaitReads = 0, AllNoticeWaitReads = 0 };
 
             InitializeComponent();
-
+            this.tabMain.DataContext = model;
             this.gChat.DataContext = chats;
             this.gFriend.DataContext = friends;
             this.gNotice.DataContext = notices;
+
+            this.listChat.AddHandler(UIElement.MouseDownEvent, new MouseButtonEventHandler(listChat_MouseLeftButtonDown), true);
         }
 
         #region 测试用按钮
         private void btnChatAddOne_Click(object sender, RoutedEventArgs e)
         {
-            ellChat.Visibility = (ellChat.Visibility == Visibility.Collapsed) ? Visibility.Visible : Visibility.Visible;
-            chats.Add(new Chat { SenderId = "8888", ReceiverId = "6666", Content = "测试测试" });
+
+            PerMsg pt1 = new PerMsg("test", "ceshi");
+            PerMsg pt2 = new PerMsg("test", "ceshi");
+            PerMsg pt3 = new PerMsg("test", "ceshi");
+            MsgList mt1 = new MsgList();
+            mt1.Add(pt1);
+            mt1.Add(pt2);
+            mt1.Add(pt3);
+
+            model.AllChatWaitReads += 1;
+            //chats.Add(new Chat { SenderId = "8888", sName = "test", ReceiverId = "1111", Content = "测试测试测试测试测试测试", Msgs = mt1 });
+            chats.Add(new Chat("8888", "test", "1111", "测试测试测试测试测试测试", mt1, 0));
         }
 
         private void btnChatDelOne_Click(object sender, RoutedEventArgs e)
@@ -65,13 +110,15 @@ namespace DataBindingDemo
 
         private void btnFriendAddOne_Click(object sender, RoutedEventArgs e)
         {
-            ellFriend.Visibility = (ellFriend.Visibility == Visibility.Collapsed) ? Visibility.Visible : Visibility.Visible;
+            //ellFriend.Visibility = (ellFriend.Visibility == Visibility.Collapsed) ? Visibility.Visible : Visibility.Visible;
+            model.AllFriendWaitReads += 1;
             friends.Add(new Friend { UserId = "8888", UserName = "测试", IP = "192.168.1.1", status = Status.Offline });
         }
 
         private void btnNoticeAddOne_Click(object sender, RoutedEventArgs e)
         {
-            ellNotice.Visibility = (ellNotice.Visibility == Visibility.Collapsed) ? Visibility.Visible : Visibility.Visible;
+            //ellNotice.Visibility = (ellNotice.Visibility == Visibility.Collapsed) ? Visibility.Visible : Visibility.Visible;
+            model.AllNoticeWaitReads += 1;
             notices.Add(new Notice { UserId="7777" });
         }
 
@@ -85,19 +132,64 @@ namespace DataBindingDemo
             notices.Remove(notices.Where(p => p.UserId.Equals("7777")).FirstOrDefault());
         }
         #endregion
+
+        private void listChat_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (listChat.SelectedIndex == -1)
+            {
+                return;
+            }
+            Chat boxItem = (Chat)listChat.SelectedItem;
+            chating = boxItem.SenderId;
+            MainChat.ItemsSource = boxItem.Msgs;
+            int x = boxItem.WaitReads;
+            model.AllChatWaitReads -= x;
+            boxItem.WaitReads = 0;
+            //MessageBox.Show(boxItem.SenderId.ToString() + "\n" + boxItem.Content);
+        }
+
+        //private void listChat_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    if (listChat.SelectedIndex == -1)
+        //    {
+        //        return;
+        //    }
+        //    Chat boxItem = (Chat)listChat.SelectedItem;
+        //    MainChat.ItemsSource = boxItem.Msgs;
+        //    //MessageBox.Show(boxItem.SenderId.ToString() + "\n" + boxItem.Content);
+        //    listChat.SelectedIndex = -1;
+        //}
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            txtSend.Text = "";
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            PerMsg p = new PerMsg("本机名字", "ceshi");
+            Chat boxItem = (Chat)listChat.SelectedItem;
+            boxItem.Msgs.Add(p);
+            txtSend.Text = "";
+        }
+
+        private void btnWangSendOne_Click(object sender, RoutedEventArgs e)
+        {
+            PerMsg mp = new PerMsg("wang", "ceshi1");
+            Chat chat = chats.Where(p => p.sName.Equals("wang")).FirstOrDefault();
+            chat.Msgs.Add(mp);
+            chat.WaitReads += 1;
+            model.AllChatWaitReads += 1;
+        }
+
+        private void btnMingSendOne_Click(object sender, RoutedEventArgs e)
+        {
+            PerMsg mp = new PerMsg("ming", "ceshi");
+            Chat chat = chats.Where(p => p.sName.Equals("ming")).FirstOrDefault();
+            chat.Msgs.Add(mp);
+            chat.WaitReads += 1;
+            model.AllChatWaitReads += 1;
+        }
     }
 
-    //[ValueConversion(typeof(string), typeof(string))]
-    //public class Id2Name : IValueConverter
-    //{
-    //    public object Convert(object value, Type TargetType)
-    //    {
-    //        if(TargetType != typeof(string)) { return null; }
-
-    //    }
-    //    public object ConvertBack(object value, Type targetType)
-    //    {
-    //        if(targetType != typeof(string)) { return null; }
-    //    }
-    //}
 }
